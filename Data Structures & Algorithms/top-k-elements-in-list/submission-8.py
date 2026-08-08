9d4cce432ca9a4c7ed09a5bcb961eab4a0f39af7
jay <lueuha@gmail.com>
2026-08-08 16:05:48 +0800

    Add: top-k-elements-in-list - submission-8

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-8.py
@@ -0,0 +1,8 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counts = {}
+        for n in nums:
+            if n not in counts:
+                counts[n]=0
+            counts[n]+=1
+        return sorted(counts,key=counts.get,reverse=True)[:k]
